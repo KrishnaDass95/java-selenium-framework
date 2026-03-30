207200f9fc0f25dcb9d48ec7d80dd358756761ff
KrishnaDass95 <krishdass95@gmail.com>
2026-03-30 21:05:58 +0530

    Make test run headless for CI

--- src/main/java/com/mystore/driver/DriverFactory.java
+++ src/main/java/com/mystore/driver/DriverFactory.java
@@ -4,6 +4,7 @@ import javax.management.RuntimeErrorException;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import com.mystore.config.ConfigReader;
 
@@ -14,13 +15,18 @@ public class DriverFactory {
     public static void initDriver(){
 
         String browser = ConfigReader.getInstance().getProperty("browser");
+        String headless = ConfigReader.getInstance().getProperty("headless");
 
         WebDriver driver;
 
         switch (browser.toLowerCase()){
             case "chrome":
+                ChromeOptions options = new ChromeOptions();
                 WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
+                if(headless.equalsIgnoreCase("true")){
+                    options.addArguments("--headless=new");
+                }
+                driver = new ChromeDriver(options);
                 break;
             
             default:
