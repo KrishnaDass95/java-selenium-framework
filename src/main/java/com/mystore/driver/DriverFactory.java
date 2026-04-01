1320422b2b02daada11988f503d0b76be66233bb
KrishnaDass95 <krishdass95@gmail.com>
2026-04-01 14:00:03 +0530

    Run tests on docker using selenium grid

--- src/main/java/com/mystore/driver/DriverFactory.java
+++ src/main/java/com/mystore/driver/DriverFactory.java
@@ -1,10 +1,17 @@
 package com.mystore.driver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javax.management.RuntimeErrorException;
 
+import org.checkerframework.checker.units.qual.C;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import com.mystore.config.ConfigReader;
 
@@ -14,35 +21,80 @@ public class DriverFactory {
 
     public static void initDriver(){
 
+        // getting info from config file
         String browser = ConfigReader.getInstance().getProperty("browser");
         String headless = ConfigReader.getInstance().getProperty("headless");
+        String executionMode = ConfigReader.getInstance().getProperty("executionMode");
 
         WebDriver driver;
 
-        switch (browser.toLowerCase()){
-            case "chrome":
-                ChromeOptions options = new ChromeOptions();
-                WebDriverManager.chromedriver().setup();
-                if(headless.equalsIgnoreCase("true")){
-                    options.addArguments("--headless=new");
-                    driver = new ChromeDriver(options);
-                }
-                else{
-                    driver = new ChromeDriver();
-                }
-                break;
-            // Todo other browsers
-            
-            default:
-                throw new RuntimeErrorException(null, "unsupported browser" + browser);
+        if (executionMode.equalsIgnoreCase("grid")){
+            driver = createRemotWebDriver(browser, headless);
         }
+        else{
+            driver = createLocalDriver(browser, headless);
+        }
+
         DriverManager.setDriver(driver);
         driver.manage().window().maximize();
+
     }
 
-    public static void quitDriver(){
-        DriverManager.getDriver().quit();
-        DriverManager.unload();
+
+    private static WebDriver createLocalDriver(String browser, String headless){
+        switch (browser.toLowerCase()){
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+                if(headless.equalsIgnoreCase("true")){
+                    options.addArguments("--headless=new");
+                }
+                return new ChromeDriver(options);
+            
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                if (headless.equalsIgnoreCase("true")) {
+                    firefoxOptions.addArguments("--headless");
+                }
+                return new FirefoxDriver(firefoxOptions);
+            default:
+                throw new RuntimeErrorException(null, "unsupported browser" + browser);
     }
-    
 }
+
+    private static WebDriver createRemotWebDriver(String browser, String headless){
+        String gridUrl = ConfigReader.getInstance().getProperty("gridUrl");
+        try {
+            switch (browser.toLowerCase()) {
+                case "chrome":
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    // Best practice for Docker grid execution
+                    chromeOptions.addArguments("--headless=new");
+                    chromeOptions.addArguments("--no-sandbox");
+                    chromeOptions.addArguments("--disable-dev-shm-usage");
+                    return new RemoteWebDriver(new URL(gridUrl), chromeOptions);
+
+                case "firefox":
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    firefoxOptions.addArguments("--headless");
+                    return new RemoteWebDriver(new URL(gridUrl), firefoxOptions);
+
+                default:
+                    throw new RuntimeException("Unsupported grid browser: " + browser);
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Grid URL is invalid: " + gridUrl, e);
+        }
+    }
+
+
+    public static void quitDriver(){
+        if (DriverManager.getDriver() != null){
+            DriverManager.getDriver().quit();
+            DriverManager.unload();
+        }
+    }
+}
+    
+
